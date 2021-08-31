b16168bdf130dca071af46e769eced45d0d684a9
Alejandro Cazorla <acazorla72@gmail.com>
2021-08-31 21:35:34 +0200

    completed iteration 7

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,41 @@
 const router = require("express").Router();
 
-// all your routes here
+const Celebrity = require("../models/Celebrity.model");
+
+const Movie = require("../models/movies.model");
+
+router.get("/create", (req, res, next) => {
+  Celebrity.find()
+    .then((allCelebrities) => {
+      res.render("movies/new-movie", { allCelebrities });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.post("/create", (req, res, next) => {
+  const { title, plot, genre, cast } = req.body;
+  //console.log({ title, plot, genre, cast });
+  Movie.create({ title, plot, genre, cast })
+    .then((newMovie) => {
+      console.log(`Here is your new movie sir ${newMovie}`);
+      res.redirect("/movies/create");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/", (req, res, next) => {
+  Movie.find()
+    .then((allMovies) => {
+      console.log(`Here are your movies sir ${allMovies}`);
+      res.render("movies/movies", { allMovies });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
